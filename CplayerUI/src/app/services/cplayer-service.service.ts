26d9c24e674d672cd8ea9ada8c01f7c82859175f
tusharV07 <85993261+tusharV07@users.noreply.github.com>
2022-06-01 10:00:00 +0530

    Update cplayer-service.service.ts
    update recommended service- added dynamic api

--- CplayerUI/src/app/services/cplayer-service.service.ts
+++ CplayerUI/src/app/services/cplayer-service.service.ts
@@ -21,19 +21,16 @@ export class CplayerServiceService {
 
   constructor(private http: HttpClient) {
     this.cplayerApi = 'https://api.cricapi.com/v1/players?';
-  this.apiKey = 'apikey=bb1861a3-6be3-4894-b8ac-b83b9183899c';
+  this.apiKey = 'apikey=';
     this.playerInfo='https://api.cricapi.com/v1/players_info?';
-     this.searchInfo = "https://api.cricapi.com/v1/players?apikey=bb1861a3-6be3-4894-b8ac-b83b9183899c&offset=0&search=";
+     this.searchInfo = "https://api.cricapi.com/v1/players?apikey=&offset=0&search=";
     this.favouritEndPoint= 'http://localhost:8083/api/v1/favoriteservice';
     this.recommendEndPoint= 'http://localhost:8082/api/v1/player/recommend/8';
 }
 getAllPlayerList(pid:String): Observable<any> {
     
-  this.apiKey = 'apikey=bb1861a3-6be3-4894-b8ac-b83b9183899c&id='+pid;
+  this.apiKey = 'apikey=&id='+pid;
 
-  // 80d32d2e-7744-4c46-b869-cf961874bada
-  // 7bbe5e7b-1d77-4a37-8b21-1572fb2e4307
-  // dafb1959-a56f-4fe2-bb9c-b5d4202d3d13
   const url = `${this.playerInfo}${this.apiKey}`;
   return this.http.get(url);
 }
@@ -41,7 +38,7 @@ getAllPlayerList(pid:String): Observable<any> {
 getPlayerDetails(pid:String):Observable<any> 
 {
  
-  this.apiKey = 'apikey=bb1861a3-6be3-4894-b8ac-b83b9183899c&id='+pid;
+  this.apiKey = 'apikey=&id='+pid;
   const url = `${this.playerInfo}${this.apiKey}`;
   return this.http.get(url);
 }
